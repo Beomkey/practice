ed46c62e1b4c0c57bb7c6d13e40714342db05274
Beom <21300356@handong.edu>
2017-10-16 17:48:30 +0900

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { TaskList } from '../pages/tasklist/tasklist';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsq7ZJKUu4QvKY9zUTdVI2TK5PXwyxiZs",
+  authDomain: "ionic2do-3e5c1.firebaseapp.com",
+  databaseURL: "https://ionic2do-3e5c1.firebaseio.com",
+  projectId: "ionic2do-3e5c1",
+  storageBucket: "",
+  messagingSenderId: "52330945566"
+};
+
 @NgModule({
-  declarations: [
+  declarations: [ 
     MyApp,
     TaskList
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController, ItemSliding } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-tasklist',
   templateUrl: 'tasklist.html'
 })
 export class TaskList {
-  tasks: Array<Task> = [];
-
-  constructor(public navCtrl: NavController) {
-    this.tasks=[
-      {title: 'Milk', status: 'open'}
-    ];
+  tasks: FirebaseListObservable<any[]>;
+  
+  constructor(public navCtrl: NavController, public af: AngularFireDatabase) {
+    this.tasks= af.list('/tasks');
   }
   
   addItem() {
@@ -21,21 +20,18 @@ export class TaskList {
     }
   }
   markAsDone(slidingItem: ItemSliding, task: Task) {
-    task.status = "done";
+    this.tasks.update(task.$key, {status: 'done'});
     slidingItem.close();
   }
-  removeTask(slidingItem: ItemSliding, task: Task) {
-    task.status = "removed";
-    let index = this.tasks.indexOf(task);
-    if (index > -1) {
-      this.tasks.splice(index, 1);
-    }
-    slidingItem.close();
 
+  removeTask(slidingItem: ItemSliding, task: Task) {
+    this.tasks.remove(task.$key);
+    slidingItem.close();
   }
 }
 
 export class Task {
   title: string;
   status: string;
+  $key: any;
 }
